ca1ca589b992b9bc757824bf73c85fbcc63726a0
guochaohong <guochaohong@ifanr.com>
2019-03-29 16:42:55 +0800

    新增是否点击后隐藏控制

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 Component({
-  properties: {},
-  data: {},
+  properties: {
+    disappearAfterClick: {
+      type: Boolean,
+      value: false,
+    }
+  },
+  data: {
+    hideButton: false,
+  },
   methods: {
     reportTicket: function (e) {
       my.BaaS.reportTicket(e.detail.formId)
     },
     handleTap: function (e) {
       this.props.onTap && this.props.onTap(e)
+      this.setData({
+        hideButton: true,
+      })
     }
   },
 })
