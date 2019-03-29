bda295447d5a3cffeab5240a7efc9a08204e5964
guochaohong <guochaohong@ifanr.com>
2019-03-29 17:45:58 +0800

    增加 SDK 版本验证

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ Component({
   data: {
     hideButton: false,
   },
+  didMount() {
+    if (typeof my.BaaS === 'undefined' || /^v?(\d+)\./i.exec(my.BaaS._config.VERSION)[1] < 2) {
+      throw new Error('请使用 v2.0 版本以上的知晓云 SDK')
+    }
+  },
   methods: {
     reportTicket: function (e) {
       my.BaaS.reportTicket(e.detail.formId)
