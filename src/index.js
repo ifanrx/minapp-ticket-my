2eb8d67b20c50bae2d040e3639c6a0f58dfce998
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-04-04 15:57:24 +0800

    使用 button 包裹

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 Component({
-  properties: {
-    disappearAfterClick: {
-      type: Boolean,
-      value: false,
-    }
-  },
-  data: {
-    hideButton: false,
-  },
   didMount() {
     if (typeof my.BaaS === 'undefined' || /^v?(\d+)\./i.exec(my.BaaS._config.VERSION)[1] < 2) {
-      throw new Error('请使用 v2.0 版本以上的知晓云 SDK')
+      throw new Error('请先接入知晓云 SDK')
     }
   },
   methods: {
     reportTicket: function (e) {
       my.BaaS.reportTicket(e.detail.formId)
     },
-    handleTap: function (e) {
-      this.props.onTap && this.props.onTap(e)
-      if (this.props.disappearAfterClick) {
-        this.setData({
-          hideButton: true,
-        })
-      }
-    }
   },
 })
